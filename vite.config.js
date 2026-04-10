4f526f625a1a062651a8b886c88f5ca8f9a916d9
Ishaan Dalvi <soham.dalviconsultancy@gmail.com>
2026-04-10 21:32:18 +0530

    Deployment: Fix 404s on sub-pages by enabling multi-page building in Vite.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ export default defineConfig({
   // Set the base path to match the GitHub Pages sub-directory name
   base: '/official/',
   build: {
-    // Ensure the output is clean
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        feed: './feed.html',
+        report: './report.html',
+        contact: './contact.html',
+        donate: './donate.html',
+      }
+    },
     outDir: 'dist',
   }
 });
